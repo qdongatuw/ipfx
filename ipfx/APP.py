ccfb18abfc2747536440c2857bcdf847e0d5837a
qdongatuw <127705040+qdongatuw@users.noreply.github.com>
2023-04-14 22:21:57 -0500

    1

--- ipfx/APP.py
+++ ipfx/APP.py
@@ -1,5 +1,26 @@
+from tkinter import filedialog
 import numpy as np
 import matplotlib.pyplot as plt
 from neo.io import AxonIO
 
 from feature_extractor import SpikeFeatureExtractor, SpikeTrainFeatureExtractor
+
+
+files = filedialog.askopenfilenames()
+
+for file in files:
+    f = AxonIO(file)
+    blocks = f.read()
+    bl = blocks[0]
+
+    num_episodes = f.segment_count(0)
+
+    if num_episodes < 3:
+        continue
+    
+    sampling_rate = f.get_signal_sampling_rate(0)
+    block_count = f.block_count()
+    segs = bl.segments
+
+    sfe = SpikeFeatureExtractor(filter=2)
+    spte = SpikeTrainFeatureExtractor(start=0, end=16000)

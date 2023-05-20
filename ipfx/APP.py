3f809146bc2e7b7d8d8af748ef3640bfd6621a22
qdongatuw <127705040+qdongatuw@users.noreply.github.com>
2023-05-20 01:37:13 -0500

    update

--- ipfx/APP.py
+++ ipfx/APP.py
@@ -13,14 +13,16 @@ sfe = SpikeFeatureExtractor(filter=2)
 spte = SpikeTrainFeatureExtractor(start=0, end=16000)
 
 out_folder = r'C:\Users\dongq\OneDrive\mouse patch-seq\result'
+out_csv = r'C:\Users\dongq\OneDrive\mouse patch-seq\result\test.csv'
 
 name_list = [os.path.basename(i) for i in files]
-df = pd.DataFrame({'File': name_list})
+df = pd.DataFrame()
 
 for file in files:
+    file_name = os.path.basename(file)
     f = pyabf.ABF(file)
 
-    if f.nOperationMode == 5:   # multi-sweeps
+    if f.nOperationMode == 5 and f.sweepCount > 3:   # multi-sweeps
         folder = out_folder + '\\' + f.abfID
         if not os.path.exists(folder):
             os.mkdir(folder)
@@ -29,7 +31,7 @@ for file in files:
 
             sampling_rate = f.sampleRate
             temp_result_list = []
-            flag_spike = False
+
             for index in f.sweepList:
                 f.setSweep(index)
                 t = f.sweepX
@@ -41,13 +43,43 @@ for file in files:
                 ft = sfe.process(t, v, i)
                 ft.to_csv(f'{folder}/{index}.csv', index=False)
                 sptft= spte.process(t=t, v=v, i=i, spikes_df=ft)
-                
+                current = f.sweepEpochs.levels[-2]
+                sptft['injected current (pA)'] = current
                 temp_result_list.append((ft, sptft))
-                
-                if not flag_spike:
-                    if sptft['avg_rate'] > 0:
-                        new_df = ft.mean().to_frame().T
-                        new_df.insert(0, 'File', os.path.basename(file))
-                        flag_spike = True
+            
+            with open(f'{folder}/result.txt', mode='w', encoding='utf-8') as result_txt_file:
+                for i_result in temp_result_list:
+                    result_txt_file.write(str(i_result[1]))
+                    result_txt_file.write('\n')
 
+            for i_result in temp_result_list:
+                if i_result[1]['avg_rate'] > 0:
+                    new_df = i_result[0].mean(numeric_only=True).to_frame().T
+                    new_df.insert(0, 'File', file_name)
+                    break
+            fire_pattern_df = None
+            for i_result in temp_result_list:
+                if 'adapt' in i_result[1] and i_result[1]['adapt'] > 0:
+                    temp_dict = {i: [i_result[1][i]] for i in i_result[1]} 
+                    fire_pattern_df = pd.DataFrame(temp_dict)
+                    fire_pattern_df.insert(0, 'File', file_name)
+                    break
+            if fire_pattern_df is not None:
+                temp_df1 = pd.merge(new_df, fire_pattern_df, on='File')
+            else:
+                temp_df1 = new_df
+            firing_rate_list = dict()
+            firing_rate_list['File'] = [file_name]
+            firing_rate_list['sampling rate'] = [sampling_rate]
+            for i_result in temp_result_list:
+                current = i_result[1]['injected current (pA)']
+                rate = i_result[1]['avg_rate'] * sampling_rate
+                firing_rate_list[f'{current} pA'] = [rate]
+            rate_df = pd.DataFrame(firing_rate_list)
+            temp_df2 = pd.merge(temp_df1, rate_df, on='File')
+            df = pd.concat([df, temp_df2])
+df.to_csv(out_csv, index=False)
+
+            
+            
             

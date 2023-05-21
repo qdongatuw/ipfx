f1c90b1ab56a9425fa2983c6383c53b6bb1861b4
qdongatuw <127705040+qdongatuw@users.noreply.github.com>
2023-05-21 16:00:08 -0500

    update

--- ipfx/demo.py
+++ ipfx/demo.py
@@ -1,72 +1,24 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import pyabf
+from tkinter import filedialog
 import os
-import pandas as pd
 
-file = r"C:\Users\dongq\OneDrive\mouse patch-seq\MBD1 project\2023_04_01_0101.abf"
-f = pyabf.ABF(file)
+# 创建一个Tkinter窗口
 
-print(f"command units: {f.sweepUnitsC}")
 
-f.setSweep(0)
-first_epoc = f.sweepEpochs
-f.setSweep(1)
-second_epoc = f.sweepEpochs
-dif = np.asarray(second_epoc.levels) - np.asarray(first_epoc.levels)
+# 弹出文件选择对话框，允许选择多个文件或文件夹
+selected_paths = filedialog.askopenfilenames()
 
-step = np.where(dif != 0)
-start = np.asarray(first_epoc.p1s)[step][0]
-end = np.asarray(first_epoc.p2s)[step][0]
+# 创建一个空的文件路径列表
+file_paths = []
 
-print(start, end)
-
-sampling_rate = f.sampleRate
-
-from feature_extractor import SpikeFeatureExtractor, SpikeTrainFeatureExtractor
-sfe = SpikeFeatureExtractor(filter=2)
-spte = SpikeTrainFeatureExtractor(start=start/sampling_rate, end=end/sampling_rate)
-
-import subthresh_features as sbth
-
-rmp = []
-tau = []
-sag = []
-
-t_set = []
-i_set = []
-v_set = []
-
-temp_result_list = []
-flag_spike = False
-for index in f.sweepList:
-    f.setSweep(index)
-    t = f.sweepX
-    v = f.sweepY
-    i = f.sweepC
-
-    if f.sweepUnitsY == f.sweepUnitsC:
-        v = v/20
-    current = np.asarray(f.sweepEpochs.levels)[step][0]
-
-    rmp.append(np.median(v[np.where(i == 0)]))
-    if current < 0:
-        baseline_interval = min(start/(sampling_rate*2), 0.1)
-        tau.append(sbth.time_constant(t=t, v=v, i=i, start=start/sampling_rate, end=(end-2)/sampling_rate, baseline_interval=baseline_interval))
-        sag.append(sbth.sag(t=t, v=v, i=i, start=start/sampling_rate, end=end/sampling_rate, baseline_interval=baseline_interval))
-        t_set.append(t)
-        i_set.append(i)
-        v_set.append(v)
-    
-    if current == 0:
-        t_set.append(t)
-        i_set.append(i)
-        v_set.append(v)
-
-    ft = sfe.process(t, v, i)
-    # ft.to_csv(f'{index}.csv', index=False)
-    sptft= spte.process(t=t, v=v, i=i, spikes_df=ft)
-    print(sptft)
-    
-    sptft['current'] = current
-    temp_result_list.append((ft, sptft))
+# 遍历选中的路径
+for path in selected_paths:
+    if os.path.isfile(path) and path.lower().endswith('.abf'):
+        # 如果路径是文件且扩展名为.abf，则直接将其添加到文件路径列表中
+        file_paths.append(path)
+    elif os.path.isdir(path):
+        # 如果路径是文件夹，则使用os.walk遍历文件夹及子文件夹下的所有.abf文件
+        for root_dir, dirs, files in os.walk(path):
+            for file in files:
+                if file.lower().endswith('.abf'):
+                    file_paths.append(os.path.join(root_dir, file))
+print(file_paths)
